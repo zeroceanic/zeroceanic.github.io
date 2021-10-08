0984bcf7601f5bb2c793963278e1b9c37329f829
zeroceanic <59518481+zeroceanic@users.noreply.github.com>
2021-10-08 11:43:19 -0500

    Add files via upload

---
+++ en/js/fuzzydata.js
@@ -0,0 +1,8 @@
+$(document).ready(function () {indexDict['en'] = [{ "title" : "PDF Embed ", 
+"url" : "pdf-embed.html", 
+"breadcrumbs" : "PDF Embed \/ PDF Embed ", 
+"snippet" : "...", 
+"body" : "[video] " }
+]
+$(document).trigger('search.ready');
+});
