00cc3112d1228bbb46496fbe84fc097b7ef62cc0
zeroceanic <59518481+zeroceanic@users.noreply.github.com>
2021-12-26 11:32:35 -0600

    Add files via upload

---
+++ festival/js/layout-custom-script.js
@@ -0,0 +1,14 @@
+$(document).ready(function () {
+    setWelcomeText();
+    $(document).ajaxComplete(function () {
+    setWelcomeText();
+    });
+});
+
+function setWelcomeText(){
+    for (var key in welcomeTextDict) {
+        var element = $("header.portal-header[data-portal-language=" + key +"] h1");
+        element.text(welcomeTextDict[key])
+    }
+}
+var welcomeTextDict = { "en" : "Archives"};
