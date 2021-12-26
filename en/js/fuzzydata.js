e2ce0ece222bf10a8057568a84b3ecaaf3cc57d3
zeroceanic <59518481+zeroceanic@users.noreply.github.com>
2021-12-26 11:13:09 -0600

    Delete en directory

--- en/js/fuzzydata.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function () {indexDict['en'] = [{ "title" : "PDF Embed ", 
-"url" : "pdf-embed.html", 
-"breadcrumbs" : "PDF Embed \/ PDF Embed ", 
-"snippet" : "...", 
-"body" : "[video] " }
-]
-$(document).trigger('search.ready');
-});
